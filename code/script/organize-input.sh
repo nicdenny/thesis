101f6d52f136e677adb3775a7ae12f5ec4db7e5c
nicdenny <nicholas.denny.94@gmail.com>
2017-12-20 18:16:15 +0100

    creating repository

---
+++ code/script/organize-input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Moving screenshots..."
+mv /home/nicholas/tesi/input/train/*.png /home/nicholas/tesi/input/train/screenshots
+
+echo "Moving bezier images..."
+mv /home/nicholas/tesi/input/train/*.jpg /home/nicholas/tesi/input/train/bezier-image
+
+echo "Moving bezier data..."
+mv /home/nicholas/tesi/input/train/*.txt /home/nicholas/tesi/input/train/bezier-data
---
+++ code/script/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/home/nicholas/tesi/code/get-film-screenshots action /home/nicholas/tesi/input/train/screenshots
+/home/nicholas/tesi/code/get-film-screenshots drama /home/nicholas/tesi/input/train/screenshots
+/home/nicholas/tesi/code/get-film-screenshots list-comedy /home/nicholas/tesi/input/train/screenshots
+/home/nicholas/tesi/code/get-film-screenshots animation /home/nicholas/tesi/input/train/screenshots
+/home/nicholas/tesi/code/get-film-screenshots action /home/nicholas/tesi/input/test/screenshots
+/home/nicholas/tesi/code/get-film-screenshots drama /home/nicholas/tesi/input/test/screenshots
+/home/nicholas/tesi/code/get-film-screenshots list-comedy /home/nicholas/tesi/input/test/screenshots
+/home/nicholas/tesi/code/get-film-screenshots animation /home/nicholas/tesi/input/test/screenshots
+python3 /home/nicholas/tesi/code/python/create_lmdb.py
+compute_image_mean -backend=lmdb /home/nicholas/tesi/input/train_lmdb /home/nicholas/tesi/input/mean.binaryproto
+caffe train -solver /home/nicholas/tesi/caffe-model/solver_1.prototext 2>&1 | tee /home/nicholas/tesi/caffe-model/model_train.log
