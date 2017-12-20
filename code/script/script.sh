1ef47a65cd4b3e938705c4841668f8c8b26f4315
nicdenny <nicholas.denny.94@gmail.com>
2017-12-20 21:37:58 +0100

    Update script.sh

--- code/script/script.sh
+++ code/script/script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-/home/nicholas/tesi/code/get-film-screenshots action /home/nicholas/tesi/input/train/screenshots
-/home/nicholas/tesi/code/get-film-screenshots drama /home/nicholas/tesi/input/train/screenshots
-/home/nicholas/tesi/code/get-film-screenshots list-comedy /home/nicholas/tesi/input/train/screenshots
-/home/nicholas/tesi/code/get-film-screenshots animation /home/nicholas/tesi/input/train/screenshots
-/home/nicholas/tesi/code/get-film-screenshots action /home/nicholas/tesi/input/test/screenshots
-/home/nicholas/tesi/code/get-film-screenshots drama /home/nicholas/tesi/input/test/screenshots
-/home/nicholas/tesi/code/get-film-screenshots list-comedy /home/nicholas/tesi/input/test/screenshots
-/home/nicholas/tesi/code/get-film-screenshots animation /home/nicholas/tesi/input/test/screenshots
-python3 /home/nicholas/tesi/code/python/create_lmdb.py
-compute_image_mean -backend=lmdb /home/nicholas/tesi/input/train_lmdb /home/nicholas/tesi/input/mean.binaryproto
-caffe train -solver /home/nicholas/tesi/caffe-model/solver_1.prototext 2>&1 | tee /home/nicholas/tesi/caffe-model/model_train.log
+#extracting screenshot from trailers for train data set
+/thesis/code/script/get-film-screenshots action /thesis/input/train/screenshots
+/thesis/code/script/get-film-screenshots drama /thesis/input/train/screenshots
+/thesis/code/script/get-film-screenshots list-comedy /thesis/input/train/screenshots
+/code/script/get-film-screenshots animation /thesis/input/train/screenshots
+
+#extracting screenshot from trailers for validation data set
+/thesis/code/script/get-film-screenshots action /thesis/input/test/screenshots
+/thesis/code/script/get-film-screenshots drama /thesis/input/test/screenshots
+/thesis/code/script/get-film-screenshots list-comedy /thesis/input/test/screenshots
+/thesis/code/script/get-film-screenshots animation /thesis/input/test/screenshots
+
+
+#python3 /home/nicholas/tesi/code/python/create_lmdb.py
+#compute_image_mean -backend=lmdb /home/nicholas/tesi/input/train_lmdb /home/nicholas/tesi/input/mean.binaryproto
+#caffe train -solver /home/nicholas/tesi/caffe-model/solver_1.prototext 2>&1 | tee /home/nicholas/tesi/caffe-model/model_train.log
